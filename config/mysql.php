8d08b930cbbfaa4fe68264d22c599207926e2115
Alex Zotov <alexandr.zotov@emory.edu>
2016-01-28 15:38:18 -0500

    fixing database

--- config/mysql.php
+++ config/mysql.php
@@ -1,4 +1,4 @@
 <?php
-$bill_link = mysqli_connect("czmysql4", "proshiv8_proshiv", "MOeXOT") or die ("cannot connect to server!");
-mysqli_select_db($bill_link, "proshiv8_fwgen") or die ("cannot select database!!");
+$bill_link = mysqli_connect("b5e79ce14cb3d1", "b6a59b96", "heroku_5c7020ebfa7ce45") or die ("cannot connect to server!");
+mysqli_select_db($bill_link, "heroku_5c7020ebfa7ce45") or die ("cannot select database!!");
 ?>
