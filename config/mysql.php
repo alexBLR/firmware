bcb3c9dbe12d291b139d69e6352346666cc5867f
Alex Zotov <alexandr.zotov@emory.edu>
2016-01-28 15:41:00 -0500

    fixing database 2

--- config/mysql.php
+++ config/mysql.php
@@ -1,4 +1,4 @@
 <?php
-$bill_link = mysqli_connect("b5e79ce14cb3d1", "b6a59b96", "heroku_5c7020ebfa7ce45") or die ("cannot connect to server!");
+$bill_link = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b6a59b96") or die ("cannot connect to server!");
 mysqli_select_db($bill_link, "heroku_5c7020ebfa7ce45") or die ("cannot select database!!");
 ?>
